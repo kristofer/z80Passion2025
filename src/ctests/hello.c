abf6327bbb144fafed3afaaf4febed1f5a7286e4
Kristofer Younger <kris@youngers.org>
2025-04-17 11:59:12 -0400

    adds cl3.bin

---
+++ src/ctests/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int count = 0;
+
+int main() {
+  puts("Hello, world!\n");
+  return 0;
+}
---
+++ src/ctests/iloop2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/* Declare a buffer for user input of size 2048 */
+static char buffer[2048];
+
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+
+int main(int argc, char** argv) {
+
+  /* Print Version and Exit Information */
+  puts("Lispy Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+
+  /* In a never ending loop */
+  while (1) {
+
+    /* Now in either case readline will be correctly defined */
+    char* input = readline("lispy> ");
+    add_history(input);
+
+    printf("No you're a %s\n", input);
+    free(input);
+
+
+  }
+
+  return 0;
+}
